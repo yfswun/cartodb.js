84799e58606ecc6eefdfb92c1775b393304a4693
Javier Arce <javierarce@gmail.com>
2014-06-11 16:19:25 +0200

    Adds image widgets

--- src/geo/ui/widget.js
+++ src/geo/ui/widget.js
@@ -1,6 +1,6 @@
-cdb.geo.ui.Header = cdb.core.View.extend({
+cdb.geo.ui.Title = cdb.core.View.extend({
 
-  className: "cartodb-widget",
+  className: "cartodb-widget widget-title",
 
   events: { },
 
@@ -14,20 +14,27 @@ cdb.geo.ui.Header = cdb.core.View.extend({
 
   },
 
-  _applyStyle: function() {
-
-    var style      = this.model.get("style");
-
-  },
+  _applyStyle: function() { },
 
   render: function() {
 
-    this.$el.html(this.template(this.model.toJSON()));
+    this.$el.html(this.template(this.model.attributes));
+
+    var extra            = this.model.get("extra");
+
+    var show_title       = extra.show_title;
+    var show_description = extra.show_description;
+
+    if ( show_title || show_description ) {
+
+      if (show_title)       this.$el.find(".text").show();
+      if (show_description) this.$el.find(".description").show();
+
+      this.$el.show();
+    }
 
     this.$text = this.$el.find(".text");
-
-    this._applyStyle();
-
+    
     return this;
 
   }
@@ -75,11 +82,72 @@ cdb.geo.ui.Widget = cdb.core.View.extend({
       maxWidth:        boxWidth
     });
 
+    var self = this;
+
+    setTimeout(function() {
+      self.$el.css({ width: self.$el.width() }) // TODO: calculate width on editing
+    }, 250)
+
   },
 
   render: function() {
 
-    this.$el.html(this.template(this.model.toJSON()));
+    this.$el.html(this.template(_.extend(this.model.attributes, { text: this.model.attributes.extra.rendered_text })));
+
+    this.$text = this.$el.find(".text");
+
+    this._applyStyle();
+
+    return this;
+
+  }
+
+});
+
+cdb.geo.ui.Image = cdb.core.View.extend({
+
+  className: "cartodb-widget image-widget",
+
+  events: { },
+
+  default_options: { },
+
+  initialize: function() {
+
+    _.defaults(this.options, this.default_options);
+
+    this.template = this.options.template;
+
+  },
+
+  _applyStyle: function() {
+
+    var style      = this.model.get("style");
+
+    var boxColor   = style["box-color"];
+    var boxOpacity = style["box-opacity"];
+    var boxWidth   = style["box-width"];
+
+    this.$text.css(style);
+
+    var rgbaCol = 'rgba(' + parseInt(boxColor.slice(-6,-4),16)
+    + ',' + parseInt(boxColor.slice(-4,-2),16)
+    + ',' + parseInt(boxColor.slice(-2),16)
+    +', ' + boxOpacity + ' )';
+
+    this.$el.css({
+      left:            this.model.get("x"),
+      top:             this.model.get("y"),
+      backgroundColor: rgbaCol
+    });
+
+    this.$el.find("img").css({ width: boxWidth });
+
+  },
+
+  render: function() {
+
+    this.$el.html(this.template(_.extend(this.model.attributes, { text: this.model.attributes.extra.rendered_text })));
 
     this.$text = this.$el.find(".text");
 
--- src/vis/overlays.js
+++ src/vis/overlays.js
@@ -25,7 +25,27 @@ cdb.vis.Overlay.register('mobile', function(data, vis) {
   return mobile.render();
 });
 
-cdb.vis.Overlay.register('header_widget', function(data, vis) {
+cdb.vis.Overlay.register('title', function(data, vis) {
+
+  var template = cdb.core.Template.compile(
+    data.template || '\
+    <div class="content">\
+    <div class="text">{{{ title }}}</div>\
+    <div class="description">{{{ description }}}</div>\
+    </div>',
+    data.templateType || 'mustache'
+  );
+
+  var widget = new cdb.geo.ui.Title({
+    model: new cdb.core.Model(JSON.parse(data.options)),
+    template: template
+  });
+
+  return widget.render();
+
+});
+
+cdb.vis.Overlay.register('image', function(data, vis) {
 
   var template = cdb.core.Template.compile(
     data.template || '\
@@ -35,7 +55,7 @@ cdb.vis.Overlay.register('header_widget', function(data, vis) {
     data.templateType || 'mustache'
   );
 
-  var widget = new cdb.geo.ui.Header({
+  var widget = new cdb.geo.ui.Image({
     model: new cdb.core.Model(JSON.parse(data.options)),
     template: template
   });
